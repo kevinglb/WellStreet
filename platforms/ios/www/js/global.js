8e48486541466d7888f4d3b84885922a7020a13a
kevinglb <kevinglbx@gmail.com>
2015-07-06 15:20:39 +0100

    1. hide search input when time wrap shows 2. css changes on ui-header

--- platforms/ios/www/js/global.js
+++ platforms/ios/www/js/global.js
@@ -12,6 +12,7 @@ function clearMap(){
 }
 
 function searchFocused(){
+	resetMapPage();
 	//detect whether the history list is visible in case that user focus(click) on the search input when searching
 	if($("#search_history_list").is(":visible")){
 		return;
@@ -21,13 +22,10 @@ function searchFocused(){
     	$(".overlay").toggle();
     	$(".arrow_wrap a").toggle();
 	}
-	
-
-     $('#search_history_list').unbind('click').on('click','li', function(){
-     	
+    $('#search_history_list').unbind('click').on('click','li', function(){
      	/*get the histroy record and load corresponding info here*/
      	resetMapPage();
-     })
+    })
 }
 
 
@@ -37,25 +35,16 @@ function resetMapPage(){
 		$("#search_history_list").slideToggle(200);
         $("#search_input").blur();
 	}
-	else{
-		return;
-	}
-	
 	// hide timepicker_wrap and show button
 	if($("#timepicker_wrap").is(":visible")){
 		$("#timepicker_wrap").slideToggle(200);
+	}
+	if($(".arrow_wrap a").is(':hidden')){
 		$(".arrow_wrap a").toggle();
 	}
-	else{
-		return;
-	}
-	
 	// hide the overlay
 	if($('.overlay').is(":visible")){
+		console.log("overlay is visible");
 		$('.overlay').toggle();
 	}
-	else{
-		return;
-	}
-	
 }
